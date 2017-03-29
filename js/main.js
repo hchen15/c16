fa6602aec5ad77c505d527bdae7ea2c558fb5327
Hannah Chen <hannahchen@Hannahs-MacBook-Pro.local>
2017-03-28 21:52:04 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+
+
+$('#landingBtn').on('click', function(){
+	window.location.href='../html/about.html';
+});
+
+$("#HarvardPresident").on('click', function(){
+	window.open("https://i-lab.harvard.edu/news/finalists-named-presidents-innovation-challenge/");
+	$(this).blur();
+});
+
+$("#HarvardRock").on('click', function(){
+	window.open("https://entrepreneurship.hbs.edu/programs/mba/Pages/rock-accelerator.aspx");
+	$(this).blur();
+});
+
+$("#email").on('click', function(){
+	window.location.href = "mailto:information@c16bio.com";
+});
+
+});
